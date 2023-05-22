6bff6e8c208f28b37dce774398c620f2dabac152
HANJUSEONG <hjs@verygoodtour.com>
2023-05-22 09:21:54 +0900

    프로젝트 초기 구성 작성

---
+++ modules/oome-web-local/src/main/java/org/oome/OomeWebLocalApplication.java
@@ -0,0 +1,12 @@
+package org.oome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OomeWebLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OomeWebLocalApplication.class, args);
+    }
+}
