d46ca8a6f7520fd1999860b6542dead6f21f3b6c
hjhearts <hjhearts@nate.com>
2023-06-14 09:37:27 +0900

    파일유틸추가 (gradle refresh 해야함)

---
+++ modules/core/oome-core-api/src/main/java/org/oome/core/api/exception/runtime/FileOomeRuntimeException.java
@@ -0,0 +1,19 @@
+package org.oome.core.api.exception.runtime;
+
+public class FileOomeRuntimeException extends BaseOomeRuntimeException {
+    public FileOomeRuntimeException() {
+        super();
+    }
+
+    public FileOomeRuntimeException(String message) {
+        super(message);
+    }
+
+    public FileOomeRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public FileOomeRuntimeException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ modules/core/oome-core-api/src/main/java/org/oome/core/api/utils/CommonFIleUtils.java
@@ -0,0 +1,63 @@
+package org.oome.core.api.utils;
+
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.lang3.RegExUtils;
+import org.oome.core.api.exception.runtime.FileOomeRuntimeException;
+import org.oome.core.utils.S;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.MessageFormat;
+import java.util.Objects;
+
+@Component
+public class CommonFIleUtils {
+
+    /**
+     * 배포환경이 linux 이기 때문에 역슬래쉬(\)를 슬래쉬(/)로 변환
+     * @param filePath 경로
+     * @return linux로 치환된 경로
+     */
+    private String replaceFileSeperator(String filePath) {
+        return RegExUtils.replaceAll(filePath, "\\\\", "/");
+    }
+
+    public String transferTo(MultipartFile mpf, String physicalPathParam){
+
+        String physicalPath = replaceFileSeperator(physicalPathParam);
+
+        File path = new File(physicalPath);
+        if(!path.exists()) {
+            if(!path.mkdirs()) {
+                throw new FileOomeRuntimeException(MessageFormat.format("디렉토리({0}) 생성 중에 오류가 발생했습니다.", physicalPath));
+            }
+        }
+
+
+        File file = new File(path, Objects.requireNonNull(mpf.getOriginalFilename()));
+
+        int num = 1;
+        while(file.exists()) {
+
+            if(num > 100) throw new FileOomeRuntimeException("중복파일 체크(100회 초과) 오류가 발생했습니다.");
+
+            file = new File(path,
+                    S.f("{0}[{1}].{2}",
+                            FilenameUtils.removeExtension(mpf.getOriginalFilename()).replaceAll("\\[[0-9]+\\]", ""),
+                            num++,
+                            FilenameUtils.getExtension(mpf.getOriginalFilename())
+                    )
+            );
+        }
+
+        try {
+            mpf.transferTo(file);
+        } catch (IllegalStateException | IOException e) {
+            throw new FileOomeRuntimeException("첨부파일 생성 중에 오류가 발생했습니다.");
+        }
+
+        return file.getName();
+    }
+}
